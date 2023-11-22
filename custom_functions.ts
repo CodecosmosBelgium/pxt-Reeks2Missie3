13a511a26282c1b4641fee666c93d43936c9fbf8
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2023-11-22 17:12:06 +0100

    Update pxt.json, custom_functions.ts, PlasticBlocks.jres

--- custom_functions.ts
+++ custom_functions.ts
@@ -1,3 +1,36 @@
+enum PlasticBlocks {
+    //% blockIdentity="blocks.custom" enumval=992 block="Black Plastic"
+    //% jres alias=BLACK_PLASTIC
+    BlackPlastic = 992,
+    //% blockIdentity="blocks.custom" enumval=991 block="Blue Plastic"
+    //% jres alias=BLUE_PLASTIC
+    BluePlastic = 991,
+    //% blockIdentity="blocks.custom" enumval=990 block="Green Plastic"
+    //% jres alias=GREEN_PLASTIC
+    GreenPlastic = 990,
+    //% blockIdentity="blocks.custom" enumval=989 block="Grey Plastic"
+    //% jres alias=GREY_PLASTIC
+    GreyPlastic = 989,
+    //% blockIdentity="blocks.custom" enumval=988 block="Orange Plastic"
+    //% jres alias=ORANGE_PLASTIC
+    OrangePlastic = 988,
+    //% blockIdentity="blocks.custom" enumval=987 block="Pink Plastic"
+    //% jres alias=PINK_PLASTIC
+    PinkPlastic = 987,
+    //% blockIdentity="blocks.custom" enumval=986 block="Purple Plastic"
+    //% jres alias=PURPLE_PLASTIC
+    PurplePlastic = 986,
+    //% blockIdentity="blocks.custom" enumval=985 block="Red Plastic"
+    //% jres alias=RED_PLASTIC
+    RedPlastic = 985,
+    //% blockIdentity="blocks.custom" enumval=984 block="White Plastic"
+    //% jres alias=WHITE_PLASTIC
+    WhitePlastic = 984,
+    //% blockIdentity="blocks.custom" enumval=983 block="Yellow Plastic"
+    //% jres alias=YELLOW_PLASTIC
+    YellowPlastic = 983
+}
+
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
     //% block="zet agent klaar"
@@ -11,8 +44,8 @@ namespace CodeCosmos {
     //% block="agent beweeg vooruit" color="#D83B01" weight=100
     export function agentMoveForward() {
         amountOfMovesUsed++;
-        const detectPostion = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
-        if (blocks.testForBlock(CONCRETE, detectPostion) && !agent.detect(AgentDetection.Block, FORWARD)) {
+        const detectPosition = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
+        if (blocks.testForBlock(YELLOW_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD) || blocks.testForBlock(ORANGE_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD)) {
             levelCheck++;
         } else {
             levelCheck = 0;
@@ -24,7 +57,7 @@ namespace CodeCosmos {
 
     //% block="agent verzamel plastic" color="#D83B01" weight=100
     export function collectPlastic() {
-        if (agent.inspect(AgentInspection.Block, FORWARD) === DIAMOND_BLOCK) {
+        if (agent.inspect(AgentInspection.Block, FORWARD) != GLASS && agent.inspect(AgentInspection.Block, FORWARD) != AIR && levelCheck == 4) {
             levelCheck++;
             agent.destroy(FORWARD);
         } else {
