98cec1e7d4f8fb5ced8c378d27d37644e723f57b
SebastiaanMoons <sebastiaan.moons@hotmail.com>
2023-12-04 14:47:20 +0100

    Update custom_functions.ts, custom_functions_NL.ts

--- custom_functions.ts
+++ custom_functions.ts
@@ -9,9 +9,7 @@ enum AgentTurn {
 namespace CodeCosmos {
     //% block="agent move forward" color="#D83B01" weight=50
     export function agentMoveForward() {
-        player.execute(`setblock 2392 46 135 air`)
-        loops.pause(10)
-        player.execute(`setblock 2392 46 135 redstone_block`)
+        player.execute(`scoreboard players set @a level_timer 0`)
         const detectPosition = world(agent.getPosition().getValue(Axis.X), agent.getPosition().getValue(Axis.Y) - 1, agent.getPosition().getValue(Axis.Z))
         if (blocks.testForBlock(YELLOW_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD) || blocks.testForBlock(ORANGE_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD) || blocks.testForBlock(GRAY_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD) || blocks.testForBlock(LIGHT_GRAY_CONCRETE, detectPosition) && !agent.detect(AgentDetection.Block, FORWARD)) {
             player.execute('scoreboard players add @a levelCheck 1')
@@ -24,17 +22,13 @@ namespace CodeCosmos {
 
     //% block="agent turn $direction" color="#D83B01" weight=49
     export function agentTurn(direction: AgentTurn) {
-        player.execute(`setblock 2392 46 135 air`)
-        loops.pause(10)
-        player.execute(`setblock 2392 46 135 redstone_block`)
+        player.execute(`scoreboard players set @a level_timer 0`)
         agent.turn(direction)
     }
 
     //% block="agent collect plastic" color="#D83B01" weight=48
     export function collectPlastic() {
-        player.execute(`setblock 2392 46 135 air`)
-        loops.pause(10)
-        player.execute(`setblock 2392 46 135 redstone_block`)
+        player.execute(`scoreboard players set @a level_timer 0`)
         if (agent.inspect(AgentInspection.Block, FORWARD) != GLASS && agent.inspect(AgentInspection.Block, FORWARD) != AIR) {
             player.execute('scoreboard players add @a levelCheck 1')
             player.execute('function levels/collectPlastic');
